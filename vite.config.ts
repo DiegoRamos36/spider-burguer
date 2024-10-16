d299b1da0898be76f45efa835620c33924e3da0f
Diego Ramos <diegoframos36@gmail.com>
2024-10-16 19:05:49 -0300

    removido algumas dependencias do https

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
-    },
     port: 5173,
   },
 });
